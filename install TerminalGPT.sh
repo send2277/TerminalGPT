9558b72d34fc1eb2d96d75f18485029e7d345d41
Send2277 <64221276+send2277@users.noreply.github.com>
2023-03-19 18:40:10 +0300

    Update install TerminalGPT.sh

--- install TerminalGPT.sh
+++ install TerminalGPT.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+apt install python3
+apt install pip
+pip install openai
 
 # Download the first file and move it to an existing directory
-wget https://github.com/send2277/TerminaGPT/blob/main/gpt
+wget https://github.com/send2277/TerminalGPT/blob/main/gpt?raw=true
+mv gpt?raw=true gpt.sh
 mv gpt /usr/local/bin/
 
 # Download the second file and create a new directory for it
-wget  https://github.com/send2277/TerminaGPT/blob/main/terminalgpt.py
+wget  https://github.com/send2277/TerminalGPT/blob/main/terminalgpt.py?raw=true
 mkdir $HOME/TerminalGPT/
+mv terminalgpt.py?raw=true terminalgpt.py
 mv terminalgpt.py $HOME/TerminalGPT/
