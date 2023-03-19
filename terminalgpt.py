0566b4df368e582825322eda06ac3663b3c5904d
Send2277 <64221276+send2277@users.noreply.github.com>
2023-03-19 18:36:35 +0300

    Update terminalgpt.py

--- terminalgpt.py
+++ terminalgpt.py
@@ -1,7 +1,7 @@
 import openai
 import json
 
-openai.api_key = "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"
+openai.api_key = "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"
 command = input()
 prompt = "i wan you to write a ubuntu terminal commad, if i will give you multiple commands , or i will ask you to do something in multiple steps you have to use && to execute all of them, but if you can to do task in one command or using two command is unnecesary - dont use &&, you also prohibited to add something to text that i provided, you have to answer linux command only, dont write anything else WRITE COMMAND ONLY, now i want you to write command to do this:" + str(command)
 response = openai.ChatCompletion.create(
